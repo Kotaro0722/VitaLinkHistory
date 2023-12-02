6231f64e2476debd94b0c4a841f6b7709c41ea09
Kotaro0722 <kotaro.tanaka@4nonome.com>
2023-12-03 02:15:07 +0900

    患者情報のテーブル作成完了

--- client/app/dashboard/page.jsx
+++ client/app/dashboard/page.jsx
@@ -1,11 +1,67 @@
 "use client";
 
-import * as React from "react";
+import React, { useState } from "react";
 import { Box, Typography } from "@mui/material";
-import EnhancedTable from "./patientTable";
+import PatientTable from "./patientTable";
 import Search from "./search";
 
 const Dashboard = () => {
+  const [data, setData] = useState([
+    {
+      id: "afgu3yarauh45",
+      name: "田中花子",
+      address: {
+        city: "茅野市",
+        town: "豊平",
+      },
+      visible: true,
+    },
+    {
+      id: "asafauehfa5",
+      name: "鈴木次郎",
+      address: {
+        city: "茅野市",
+        town: "長倉",
+      },
+      visible: true,
+    },
+    {
+      id: "gaseritaser",
+      name: "佐藤太郎",
+      address: {
+        city: "茅野市",
+        town: "宮川",
+      },
+      visible: true,
+    },
+    {
+      id: "gajoeirt",
+      name: "井上太一",
+      address: {
+        city: "茅野市",
+        town: "塚原",
+      },
+      visible: true,
+    },
+    {
+      id: "saleirlueer",
+      name: "川崎美也子",
+      address: {
+        city: "茅野市",
+        town: "米沢",
+      },
+      visible: true,
+    },
+    {
+      id: "sakehrerr",
+      name: "本田正敏",
+      address: {
+        city: "諏訪市",
+        town: "四賀",
+      },
+      visible: true,
+    },
+  ]);
   return (
     <Box
       sx={{
@@ -23,8 +79,8 @@ const Dashboard = () => {
       >
         患者情報一覧
       </Typography>
-      <Search />
-      <EnhancedTable />
+      <Search data={data} setData={setData} />
+      <PatientTable data={data} />
     </Box>
   );
 };
--- client/app/dashboard/search.jsx
+++ client/app/dashboard/search.jsx
@@ -2,7 +2,7 @@
 
 import { ThemeProvider } from "@emotion/react";
 import { Box, Button, Grid, TextField, createTheme } from "@mui/material";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const theme = createTheme({
   components: {
@@ -17,22 +17,53 @@ const theme = createTheme({
   },
 });
 
-const Search = () => {
+const Search = ({ data, setData }) => {
+  const [name, setName] = useState("");
+  const [city, setCity] = useState("");
+  const [town, setTown] = useState("");
+
+  useEffect(() => {
+    const newData = data.map((datum) => {
+      return (!name || datum.name.includes(name)) &&
+        (!city || city == datum.address.city) &&
+        (!town || town == datum.address.town)
+        ? {
+            id: datum.id,
+            name: datum.name,
+            address: datum.address,
+            visible: true,
+          }
+        : {
+            id: datum.id,
+            name: datum.name,
+            address: datum.address,
+            visible: false,
+          };
+    });
+    setData(newData);
+  }, [name, city, town]);
+
   return (
     <Box>
       <ThemeProvider theme={theme}>
         <Grid container spacing={1} justifyContent="center" alignItems="center">
           <Grid item>
-            <TextField label="氏名" />
+            <TextField
+              label="氏名"
+              onChange={(value) => setName(value.currentTarget.value)}
+            />
           </Grid>
           <Grid item>
-            <TextField label="市町村名" />
+            <TextField
+              label="市町村名"
+              onChange={(value) => setCity(value.currentTarget.value)}
+            />
           </Grid>
           <Grid item>
-            <TextField label="地域名" />
-          </Grid>
-          <Grid item>
-            <Button variant="contained">検索</Button>
+            <TextField
+              label="地域名"
+              onChange={(value) => setTown(value.currentTarget.value)}
+            />
           </Grid>
         </Grid>
       </ThemeProvider>

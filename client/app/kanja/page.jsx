f66f28c5cd1fe02b8fa70590f1f66dcbe2c72d13
mutsuki <mtkkbyma7079@gmail.com>
2023-12-02 18:06:42 +0900

    kanja

--- client/app/kanja/page.jsx
+++ client/app/kanja/page.jsx
@@ -14,23 +14,10 @@ import Grid from "@mui/material/Grid";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
 
 function Copyright(props) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-         Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
+  
 }
 
 // TODO remove, this demo shouldn't need to reset the theme.
@@ -51,63 +38,59 @@ export default function SignInSide() {
     <ThemeProvider theme={defaultTheme}>
       <Grid container component="main" sx={{ height: "100vh" }}>
         <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage:
-              "url(https://source.unsplash.com/random?wallpapers)",
-            backgroundRepeat: "no-repeat",
-            backgroundColor: (t) =>
-              t.palette.mode === "light"
-                ? t.palette.grey[50]
-                : t.palette.grey[900],
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Grid item xs={12} sm={20} md={5} component={Paper} elevation={6} square>
           <Box
             sx={{
-              my: 8,
+              my: 20,
               mx: 4,
-              display: "flex",
+              display: "flow",
               flexDirection: "column",
               alignItems: "center",
             }}
+            
           >
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
             <Box
               component="form"
               noValidate
               onSubmit={handleSubmit}
               sx={{ mt: 1 }}
             >
-              
-              
-              
               <Button
                 type="submit"
-                fullWidth
+                size="large"
                 variant="contained"
-                sx={{ mt: 3, mb: 2 }}
+                sx={{ mt: 5,
+                   mb: 7,
+                   width:"400px",
+                   height:"500px",
+                   backgroundImage:
+                   "url(https://pictogram2.com/p/p0358/i/tn.png)", 
+                   backgroundRepeat: "no-repeat",
+                  backgroundColor:"white",
+                  backgroundPosition:"bottom",
+                  backgroundSize:"200px",
+                color:"green",
+              fontSize:"50px",}}
+                 
               >
                 患者の記録
               </Button>
               <Button
                 type="submit"
                 fullWidth
+                size="large"
                 variant="contained"
-                sx={{ mt: 3, mb: 2 }}
+                sx={{ mt: 3,
+                   mb: 2,
+                   width:"400px",
+                   height:"100px",
+                   backgroundColor:"white",
+                   color:"green",
+                   fontSize:"30px" }}
+                
               >
                患者からの質問
+
               </Button>
               
               <Copyright sx={{ mt: 5 }} />

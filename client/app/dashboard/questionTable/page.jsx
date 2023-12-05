a95b1e5fde66333ea4ee9950177183bdadd21fd5
Kotaro0722 <kotaro.tanaka@4nonome.com>
2023-12-06 02:00:13 +0900

    大体の見た目作成

--- client/app/dashboard/questionTable/page.jsx
+++ client/app/dashboard/questionTable/page.jsx
@@ -1,80 +1,72 @@
+import QuestionTable from "@/components/dashboard/question/questionTable";
+import { Box, Typography } from "@mui/material";
+
 const quesitionList = [
   {
     id: "afgu3yarauh45",
     name: "田中花子",
     content: "農作業で腰を痛めて歩けない。",
-    post_date: {
-      year: "2023",
-      month: "12",
-      day: "5",
-      hour: "20",
-      minutes: "39",
-    },
+    post_date: "2023/12/06/1:26",
+    isRead: false,
   },
   {
     id: "asafauehfa5",
     name: "鈴木次郎",
     content: "薬の飲み方がわからない。",
-    post_date: {
-      year: "2023",
-      month: "12",
-      day: "5",
-      hour: "20",
-      minutes: "39",
-    },
+    post_date: "2023/12/06/1:26",
+    isRead: false,
   },
   {
     id: "gaseritaser",
     name: "佐藤太郎",
     content: "いつまで生きられそう？",
-    post_date: {
-      year: "2023",
-      month: "12",
-      day: "5",
-      hour: "20",
-      minutes: "39",
-    },
+    post_date: "2023/12/06/1:26",
+    isRead: false,
   },
   {
     id: "gajoeirt",
     name: "井上太一",
     content: "死んだらどうなる",
-    post_date: {
-      year: "2023",
-      month: "12",
-      day: "5",
-      hour: "20",
-      minutes: "39",
-    },
+    post_date: "2023/12/06/1:26",
+    isRead: false,
   },
   {
     id: "saleirlueer",
     name: "川崎美也子",
-    content: "",
-    post_date: {
-      year: "2023",
-      month: "12",
-      day: "5",
-      hour: "20",
-      minutes: "39",
-    },
+    content: "風邪っぽい",
+    post_date: "2023/12/06/1:26",
+    isRead: false,
   },
   {
     id: "sakehrerr",
     name: "本田正敏",
-    content: "農作業で腰を痛めて歩けない。",
-    post_date: {
-      year: "2023",
-      month: "12",
-      day: "5",
-      hour: "20",
-      minutes: "39",
-    },
+    content: "次の病院はいつ行けばいい？",
+    post_date: "2023/12/06/1:26",
+    isRead: false,
   },
 ];
 
-const QuestionTable = () => {
-  return null;
+const Question = () => {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "100vh",
+        backgroundColor: "#9BC3FF",
+      }}
+    >
+      <Typography
+        sx={{
+          fontSize: "45px",
+          pt: "20px",
+          pl: "20px",
+        }}
+      >
+        患者からの質問
+      </Typography>
+      <QuestionTable data={quesitionList} />
+    </Box>
+  );
 };
 
-export default QuestionTable;
+export default Question;

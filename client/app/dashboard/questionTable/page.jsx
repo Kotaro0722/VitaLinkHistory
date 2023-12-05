0bda4e071968ea8423a76be3bdf13f53a6e9116b
TanakaKotaro <kotaro.tanaka@4nonome.com>
2023-12-06 00:36:24 +0900

    患者からの質問 表を作成中

---
+++ client/app/dashboard/questionTable/page.jsx
@@ -0,0 +1,80 @@
+const quesitionList = [
+  {
+    id: "afgu3yarauh45",
+    name: "田中花子",
+    content: "農作業で腰を痛めて歩けない。",
+    post_date: {
+      year: "2023",
+      month: "12",
+      day: "5",
+      hour: "20",
+      minutes: "39",
+    },
+  },
+  {
+    id: "asafauehfa5",
+    name: "鈴木次郎",
+    content: "薬の飲み方がわからない。",
+    post_date: {
+      year: "2023",
+      month: "12",
+      day: "5",
+      hour: "20",
+      minutes: "39",
+    },
+  },
+  {
+    id: "gaseritaser",
+    name: "佐藤太郎",
+    content: "いつまで生きられそう？",
+    post_date: {
+      year: "2023",
+      month: "12",
+      day: "5",
+      hour: "20",
+      minutes: "39",
+    },
+  },
+  {
+    id: "gajoeirt",
+    name: "井上太一",
+    content: "死んだらどうなる",
+    post_date: {
+      year: "2023",
+      month: "12",
+      day: "5",
+      hour: "20",
+      minutes: "39",
+    },
+  },
+  {
+    id: "saleirlueer",
+    name: "川崎美也子",
+    content: "",
+    post_date: {
+      year: "2023",
+      month: "12",
+      day: "5",
+      hour: "20",
+      minutes: "39",
+    },
+  },
+  {
+    id: "sakehrerr",
+    name: "本田正敏",
+    content: "農作業で腰を痛めて歩けない。",
+    post_date: {
+      year: "2023",
+      month: "12",
+      day: "5",
+      hour: "20",
+      minutes: "39",
+    },
+  },
+];
+
+const QuestionTable = () => {
+  return null;
+};
+
+export default QuestionTable;

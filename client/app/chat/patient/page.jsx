683bc4f6c2bd789cfa48af62eb2962e8812ad329
Kotaro0722 <kotaro.tanaka@4nonome.com>
2023-12-09 15:58:11 +0900

    修正

--- client/app/chat/patient/page.jsx
+++ client/app/chat/patient/page.jsx
@@ -1,10 +1,11 @@
 import { Box } from "@mui/material";
 
-const Chat=({name})=>{
-    return <Box>
-        <h1>{name}
-        </h1>
-    </Box>;
-}
+const Chat = ({ name }) => {
+  return (
+    <Box>
+      <h1>{name}</h1>
+    </Box>
+  );
+};
 
 export default Chat;

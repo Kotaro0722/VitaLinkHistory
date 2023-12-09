40561fea811228ef8db760a4644d920625ca0a30
Kotaro0722 <kotaro.tanaka@4nonome.com>
2023-12-10 02:30:27 +0900

    作成中

--- client/app/chat/patient/[id]/page.jsx
+++ client/app/chat/patient/[id]/page.jsx
@@ -1,59 +1,70 @@
 "use client";
 
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { useState } from "react";
-
-const [quesitionList, setQuestionList] = useState([
-  {
-    id: "afgu3yarauh45",
-    name: "田中花子",
-    content: "農作業で腰を痛めて歩けない。",
-    post_date: "2023/12/06/01:26",
-    isRead: false,
-  },
-  {
-    id: "asafauehfa5",
-    name: "鈴木次郎",
-    content: "薬の飲み方がわからない。",
-    post_date: "2023/12/06/01:26",
-    isRead: false,
-  },
-  {
-    id: "gaseritaser",
-    name: "佐藤太郎",
-    content: "いつまで生きられそう？",
-    post_date: "2023/12/06/01:26",
-    isRead: false,
-  },
-  {
-    id: "gajoeirt",
-    name: "井上太一",
-    content: "死んだらどうなる",
-    post_date: "2023/12/06/01:26",
-    isRead: false,
-  },
-  {
-    id: "saleirlueer",
-    name: "川崎美也子",
-    content: "風邪っぽい",
-    post_date: "2023/12/06/01:26",
-    isRead: false,
-  },
-  {
-    id: "sakehrerr",
-    name: "本田正敏",
-    content: "次の病院はいつ行けばいい？",
-    post_date: "2023/12/06/01:26",
-    isRead: false,
-  },
-]);
+import chat from "@/components/chat/chat";
 
 const Chat = ({ params }) => {
+  const [quesitionList, setQuestionList] = useState([
+    {
+      id: "afgu3yarauh45",
+      name: "田中花子",
+      content: "農作業で腰を痛めて歩けない。",
+      post_date: "2023/12/06/01:26",
+      isRead: false,
+    },
+    {
+      id: "asafauehfa5",
+      name: "鈴木次郎",
+      content: "薬の飲み方がわからない。",
+      post_date: "2023/12/06/01:26",
+      isRead: false,
+    },
+    {
+      id: "gaseritaser",
+      name: "佐藤太郎",
+      content: "いつまで生きられそう？",
+      post_date: "2023/12/06/01:26",
+      isRead: false,
+    },
+    {
+      id: "gajoeirt",
+      name: "井上太一",
+      content: "死んだらどうなる",
+      post_date: "2023/12/06/01:26",
+      isRead: false,
+    },
+    {
+      id: "saleirlueer",
+      name: "川崎美也子",
+      content: "風邪っぽい",
+      post_date: "2023/12/06/01:26",
+      isRead: false,
+    },
+    {
+      id: "sakehrerr",
+      name: "本田正敏",
+      content: "次の病院はいつ行けばいい？",
+      post_date: "2023/12/06/01:26",
+      isRead: false,
+    },
+  ]);
+  const questionHistory = [{}];
   const id = params.id;
-  const patientData = quesitionList.filter((datum) => datum.id == id);
+  const patientData = quesitionList.filter((datum) => datum.id == id)[0];
+  console.log(patientData);
   return (
-    <Box>
-      <Typography>{patientData.name}</Typography>
+    <Box sx={{ backgroundColor: "black", height: "100vh" }}>
+      <Typography
+        sx={{
+          backgroundColor: "dimgray",
+          color: "white",
+          textAlign: "center",
+          fontSize: "30px",
+        }}
+      >
+        {patientData.name}さん
+      </Typography>
     </Box>
   );
 };

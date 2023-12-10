f3d2dc59059df027cdf54a300153c36d889083b1
Kotaro0722 <kotaro.tanaka@4nonome.com>
2023-12-10 13:01:09 +0900

    チャットのスタイル修正

--- client/components/chat/chat.jsx
+++ client/components/chat/chat.jsx
@@ -1,10 +1,19 @@
 import { Typography } from "@mui/material";
 
-const chat = ({ text, isMine }) => {
+const Chat = ({ text, isMine }) => {
   return (
     <Typography
       sx={{
-        backgroundColor: isMine ? "green" : "white",
+        backgroundColor: isMine ? "lime" : "dimgray",
+        width: "fit-content",
+        maxWidth: "600px",
+        margin: isMine ? "0 20px 0 auto" : "0 auto 0 20px",
+        fontSize: "30px",
+        border: "1px solid" + isMine ? "green" : "black",
+        borderRadius: "26px",
+        p: "3px",
+        color: isMine ? "black" : "white",
+        my: "10px",
       }}
     >
       {text}
@@ -12,4 +21,4 @@ const chat = ({ text, isMine }) => {
   );
 };
 
-export default chat;
+export default Chat;

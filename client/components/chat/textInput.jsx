b0de453902b480047a2d9dea8b12f4339d1dbc11
Kotaro0722 <kotaro.tanaka@4nonome.com>
2023-12-10 14:48:23 +0900

    入力欄を作成中

---
+++ client/components/chat/textInput.jsx
@@ -0,0 +1,12 @@
+import { TextField } from "@mui/material";
+
+const TextInput = () => {
+  return (
+    <TextField
+      id="outlined-basic"
+      sx={{ width: "100%", backgroundColor: "white" }}
+    />
+  );
+};
+
+export default TextInput;

3c14864be15504bd71b942beadd9d7b3986ed3b3
Kotaro0722 <kotaro.tanaka@4nonome.com>
2023-12-12 01:07:45 +0900

    入力したメッセージを取得可能

--- client/components/chat/textInput.jsx
+++ client/components/chat/textInput.jsx
@@ -1,19 +1,39 @@
-import { TextField } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import { useRef } from "react";
 
-const TextInput = () => {
-  const textRef = useRef();
+const TextInput = ({ setMessage }) => {
+  const textRef = useRef(null);
+
+  const handleSubmit = (message) => {
+    console.log(message);
+  };
+
   return (
-    <TextField
-      id="outlined-basic"
-      sx={{
-        width: "100%",
-        backgroundColor: "white",
-        position: "fixed",
-        bottom: 0,
-      }}
-      ref={textRef}
-    />
+    <>
+      <TextField
+        id="outlined-basic"
+        sx={{
+          // width: "100%",
+          backgroundColor: "white",
+          position: "fixed",
+          bottom: 0,
+        }}
+        inputRef={textRef}
+      />
+      <Button
+        sx={{
+          position: "fixed",
+          bottom: 20,
+          right: 20,
+          backgroundColor: "black",
+          color: "white",
+          fontSize: "20px",
+        }}
+        onClick={() => handleSubmit(textRef.current.value)}
+      >
+        送信
+      </Button>
+    </>
   );
 };
 

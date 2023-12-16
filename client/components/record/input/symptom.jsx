aed074d06d164164561fc1e880b9e11901d7c6e0
Kotaro0722 <kotaro.tanaka@4nonome.com>
2023-12-16 16:31:57 +0900

    症状の記入欄作成

---
+++ client/components/record/input/symptom.jsx
@@ -0,0 +1,39 @@
+import {
+  Paper,
+  TextField,
+  ThemeProvider,
+  Typography,
+  createTheme,
+} from "@mui/material";
+
+const theme = createTheme({
+  components: {
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          height: "100%",
+        },
+      },
+    },
+  },
+});
+
+const Symptom = ({ title }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Paper sx={{ px: "20px", height: "30%" }}>
+        <Typography sx={{ fontSize: "25px" }}>{title}</Typography>
+        <TextField
+          sx={{
+            width: "100%",
+            height: "calc(100% - 37.5px - 20px)",
+          }}
+          multiline
+          rows={7}
+        />
+      </Paper>
+    </ThemeProvider>
+  );
+};
+
+export default Symptom;

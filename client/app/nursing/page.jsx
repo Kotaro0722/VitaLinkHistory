6b6ecee4bed1c1095d628ba97e94f6d079d81d1a
Kotaro0722 <kotaro.tanaka@4nonome.com>
2023-12-18 01:19:31 +0900

    mapで書き換え

--- client/app/nursing/page.jsx
+++ client/app/nursing/page.jsx
@@ -6,114 +6,80 @@ import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-const defaultTheme = createTheme();
-
-export default function SignInSide() {
+const Page = () => {
+  const lists = [
+    "大波暁斗（✕✕介護センター）",
+    "大波暁斗（△△介護センター）",
+    "大波暁斗（〇〇介護センター）",
+    " 大波暁斗（〇〇介護センター）",
+  ];
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Grid
-        container
-        component="main"
-        sx={{ height: "150vh", backgroundColor: "white" }}
+    <Grid
+      container
+      component="main"
+      sx={{ height: "150vh", backgroundColor: "white" }}
+    >
+      <Box
+        sx={{
+          display: "flow",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
       >
         <Box
           sx={{
-            display: "flow",
-            flexDirection: "column",
-            alignItems: "center",
+            mt: 5,
+            mb: 7,
+            width: "100vw",
+            height: "10vh",
+            backgroundColor: "blue",
+            color: "white",
+            textAlign: "center",
+            fontSize: "40px",
+            margin: "0 0 0 0",
+            position: "fixed",
           }}
         >
-          <Box sx={{}}>
-            <Box
-              sx={{
-                mt: 5,
-                mb: 7,
-                width: "100vw",
-                height: "10vh",
-                backgroundColor: "blue",
-                color: "white",
-                textAlign: "center",
-                fontSize: "40px",
-                margin: "0 0 0 0",
-                position: "fixed",
-              }}
-            >
-              介護サービス事業者
-            </Box>
-          </Box>
-          <Box sx={{ paddingTop: "5%" }}>
-            <Box
-              sx={{
-                width: "100vw",
-                height: "30vh",
-                backgroundColor: "white",
-                color: "green",
-                textAlign: "center",
-                fontSize: "35px",
-                border: "3px solid black",
-                lineHeight: "30vh",
-              }}
-            >
-              大波暁斗（✕✕介護センター）
-            </Box>
-            <Box
-              sx={{
-                width: "100vw",
-                height: "30vh",
-                backgroundColor: "white",
-                color: "green",
-                textAlign: "center",
-                fontSize: "35px",
-                border: "3px solid black",
-                lineHeight: "30vh",
-              }}
-            >
-              大波暁斗（△△介護センター）
-            </Box>
-            <Box
-              sx={{
-                width: "100vw",
-                height: "30vh",
-                backgroundColor: "white",
-                color: "green",
-                textAlign: "center",
-                fontSize: "35px",
-                border: "3px solid black",
-                lineHeight: "30vh",
-              }}
-            >
-              大波暁斗（〇〇介護センター）
-            </Box>
-            <Box
-              sx={{
-                width: "100vw",
-                height: "30vh",
-                backgroundColor: "white",
-                color: "green",
-                textAlign: "center",
-                fontSize: "35px",
-                border: "3px solid black",
-                lineHeight: "30vh",
-              }}
-            >
-              大波暁斗（〇〇介護センター）
-            </Box>
-            <Button
-              type="submit"
-              variant="contained"
-              sx={{
-                mt: 3,
-                mb: 2,
-                width: "100vw",
-                height: "10vh",
-                fontSize: "30px",
-              }}
-            >
-              チャット
-            </Button>
-          </Box>
+          介護サービス事業者
         </Box>
-      </Grid>
-    </ThemeProvider>
+        <Box sx={{ paddingTop: "5%" }}>
+          {lists.map((list, index) => {
+            return (
+              <Box
+                sx={{
+                  width: "100vw",
+                  height: "30vh",
+                  backgroundColor: "white",
+                  color: "green",
+                  textAlign: "center",
+                  fontSize: "35px",
+                  border: "3px solid black",
+                  lineHeight: "30vh",
+                }}
+                key={index}
+              >
+                {list}
+              </Box>
+            );
+          })}
+
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{
+              mt: 3,
+              mb: 2,
+              width: "100vw",
+              height: "10vh",
+              fontSize: "30px",
+            }}
+          >
+            チャット
+          </Button>
+        </Box>
+      </Box>
+    </Grid>
   );
-}
+};
+
+export default Page;

3c0d5e047571df57cfa8583ada01b5f54f40ef46
mutsuki <mtkkbyma7079@gmail.com>
2023-12-17 21:43:27 +0900

    nursing

--- client/app/nursing/page.jsx
+++ client/app/nursing/page.jsx
@@ -1,44 +1,20 @@
 "use client";
 
 import * as React from "react";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-// import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-
-function Copyright(props) {}
-
-// TODO remove, this demo shouldn't need to reset the theme.
 
 const defaultTheme = createTheme();
 
 export default function SignInSide() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
-  };
   return (
     <ThemeProvider theme={defaultTheme}>
       <Grid
         container
         component="main"
-        sx={{ height: "200vh", backgroundColor: "white" }}
+        sx={{ height: "150vh", backgroundColor: "white" }}
       >
-        {/* <CssBaseline /> */}
-        {/* <Grid item xs={12} sm={20} md={5} component={Paper} elevation={6} square> */}
         <Box
           sx={{
             display: "flow",
@@ -46,27 +22,17 @@ export default function SignInSide() {
             alignItems: "center",
           }}
         >
-          <Box
-            // component="form"
-            // noValidate
-            onSubmit={handleSubmit}
-            sx={{}}
-          >
+          <Box sx={{}}>
             <Box
               sx={{
                 mt: 5,
                 mb: 7,
-                width: "80vw",
-                height: "20vh",
-                backgroundImage: "url()",
-                backgroundRepeat: "no-repeat",
+                width: "100vw",
+                height: "10vh",
                 backgroundColor: "blue",
-                backgroundPosition: "bottom",
-                backgroundSize: "250px",
                 color: "white",
                 textAlign: "center",
                 fontSize: "40px",
-                lineHeight: "20vh",
                 margin: "0 0 0 0",
                 position: "fixed",
               }}
@@ -74,23 +40,12 @@ export default function SignInSide() {
               介護サービス事業者
             </Box>
           </Box>
-          <Box
-            // component="form"
-            // noValidate
-            onSubmit={handleSubmit}
-            sx={{ paddingTop: "20%" }}
-          >
+          <Box sx={{ paddingTop: "5%" }}>
             <Box
               sx={{
-                // mt: 5,
-                // mb: 7,
-                width: "80vw",
+                width: "100vw",
                 height: "30vh",
-                backgroundImage: "url()",
-                backgroundRepeat: "no-repeat",
                 backgroundColor: "white",
-                backgroundPosition: "bottom",
-                backgroundSize: "250px",
                 color: "green",
                 textAlign: "center",
                 fontSize: "35px",
@@ -102,15 +57,9 @@ export default function SignInSide() {
             </Box>
             <Box
               sx={{
-                // mt: 5,
-                // mb: 7,
-                width: "80vw",
+                width: "100vw",
                 height: "30vh",
-                backgroundImage: "url()",
-                backgroundRepeat: "no-repeat",
                 backgroundColor: "white",
-                backgroundPosition: "bottom",
-                backgroundSize: "250px",
                 color: "green",
                 textAlign: "center",
                 fontSize: "35px",
@@ -122,15 +71,9 @@ export default function SignInSide() {
             </Box>
             <Box
               sx={{
-                // mt: 5,
-                // mb: 7,
-                width: "80vw",
+                width: "100vw",
                 height: "30vh",
-                backgroundImage: "url()",
-                backgroundRepeat: "no-repeat",
                 backgroundColor: "white",
-                backgroundPosition: "bottom",
-                backgroundSize: "250px",
                 color: "green",
                 textAlign: "center",
                 fontSize: "35px",
@@ -142,15 +85,9 @@ export default function SignInSide() {
             </Box>
             <Box
               sx={{
-                // mt: 5,
-                // mb: 7,
-                width: "80vw",
+                width: "100vw",
                 height: "30vh",
-                backgroundImage: "url()",
-                backgroundRepeat: "no-repeat",
                 backgroundColor: "white",
-                backgroundPosition: "bottom",
-                backgroundSize: "250px",
                 color: "green",
                 textAlign: "center",
                 fontSize: "35px",
@@ -162,7 +99,6 @@ export default function SignInSide() {
             </Box>
           </Box>
         </Box>
-        {/* </Grid> */}
       </Grid>
     </ThemeProvider>
   );

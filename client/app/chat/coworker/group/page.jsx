7b66ba7310b4058dacfc895284bc0e26b322872f
Kotaro0722 <kotaro.tanaka@4nonome.com>
2023-12-18 02:27:11 +0900

    groupでの機能作成

--- client/app/chat/coworker/group/page.jsx
+++ client/app/chat/coworker/group/page.jsx
@@ -1,181 +1,141 @@
 "use client";
 
-import * as React from "react";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-// import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Paper from "@mui/material/Paper";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useState } from "react";
+import { Box, Button, Typography } from "@mui/material";
+import Search from "@/components/chat/list/group/search";
 
-function Copyright(props) {}
+const Page = () => {
+  const [lists, setLists] = useState([
+    {
+      name: "鈴木太郎",
+      belong: "茅野中央病院",
+      isVisible: true,
+      isSelected: false,
+    },
+    {
+      name: "田中花子",
+      belong: "岡谷西医院",
+      isVisible: true,
+      isSelected: false,
+    },
+    {
+      name: "源次郎",
+      belong: "諏訪東診療所",
+      isVisible: true,
+      isSelected: false,
+    },
+    {
+      name: "佐藤史郎",
+      belong: "富士見南総合病院",
+      isVisible: true,
+      isSelected: false,
+    },
+    {
+      name: "伊藤佳代子",
+      belong: "茅野中央介護センター",
+      isVisible: true,
+      isSelected: false,
+    },
+    {
+      name: "東南",
+      belong: "岡谷西介護センター",
+      isVisible: true,
+      isSelected: false,
+    },
+    {
+      name: "永井敬",
+      belong: "諏訪東介護センター",
+      isVisible: true,
+      isSelected: false,
+    },
+    {
+      name: "本田翼",
+      belong: "富士見南介護センター",
+      isVisible: true,
+      isSelected: false,
+    },
+  ]);
 
-// TODO remove, this demo shouldn't need to reset the theme.
+  const listsLength = lists.filter((list) => list.isVisible).length;
+  const height = "100px";
 
-const defaultTheme = createTheme();
-
-export default function SignInSide() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+  const onClick = (selectIndex) => {
+    const newList = {
+      name: lists[selectIndex].name,
+      belong: lists[selectIndex].belong,
+      isVisible: lists[selectIndex].isVisible,
+      isSelected: !lists[selectIndex].isSelected,
+    };
+    setLists(
+      lists.map((list, index) => (selectIndex == index ? newList : list))
+    );
   };
+
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Grid
-        container
-        component="main"
-        sx={{ height: "300vh", backgroundColor: "white" }}
+    <Box sx={{ height: "100vh", backgroundColor: "white", pb: "5px" }}>
+      <Box
+        sx={{
+          backgroundColor: "blue",
+          color: "white",
+          textAlign: "center",
+          fontSize: "40px",
+          mb: "20px",
+        }}
       >
-        {/* <CssBaseline /> */}
-        {/* <Grid item xs={12} sm={20} md={5} component={Paper} elevation={6} square> */}
-        <Box
-          sx={{
-            display: "flow",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Box
-            // component="form"
-            // noValidate
-            onSubmit={handleSubmit}
-            sx={{ backgroundAttachment: "fixed" }}
-          >
-            <Box
-              sx={{
-                mt: 5,
-                mb: 7,
-                width: "80vw",
-                height: "20vh",
-                backgroundImage: "url()",
-                backgroundRepeat: "no-repeat",
-                backgroundColor: "blue",
-                backgroundPosition: "bottom",
-                backgroundSize: "250px",
-
-                color: "white",
-                textAlign: "center",
-                fontSize: "40px",
-                lineHeight: "20vh",
-              }}
-            >
-              グループ作成
-            </Box>
-            <Box
-              sx={{
-                mt: 5,
-                mb: 7,
-                width: "80vw",
-                height: "20vh",
-                backgroundImage: "url()",
-                backgroundRepeat: "no-repeat",
-                backgroundColor: "blue",
-                backgroundPosition: "bottom",
-                backgroundSize: "250px",
-
-                color: "white",
-                textAlign: "center",
-                fontSize: "40px",
-                lineHeight: "20vh",
-              }}
-            >
-              選択してください
-            </Box>
-            <Box
-              sx={{
-                mt: 5,
-                mb: 7,
-                width: "80vw",
-                height: "30vh",
-                backgroundImage: "url()",
-                backgroundRepeat: "no-repeat",
-                backgroundColor: "white",
-                backgroundPosition: "bottom",
-                backgroundSize: "250px",
-                color: "green",
-                textAlign: "center",
-                fontSize: "35px",
-                border: "3px solid black",
-                lineHeight: "30vh",
-              }}
-            >
-              大波暁斗（✕✕医院）
-            </Box>
-            <Box
-              sx={{
-                mt: 5,
-                mb: 7,
-                width: "80vw",
-                height: "30vh",
-                backgroundImage: "url()",
-                backgroundRepeat: "no-repeat",
-                backgroundColor: "white",
-                backgroundPosition: "bottom",
-                backgroundSize: "250px",
-                color: "green",
-                textAlign: "center",
-                fontSize: "35px",
-                border: "3px solid black",
-                lineHeight: "30vh",
-              }}
-            >
-              大波暁斗（△△医院）
-            </Box>
-            <Box
-              sx={{
-                mt: 5,
-                mb: 7,
-                width: "80vw",
-                height: "30vh",
-                backgroundImage: "url()",
-                backgroundRepeat: "no-repeat",
-                backgroundColor: "white",
-                backgroundPosition: "bottom",
-                backgroundSize: "250px",
-                color: "green",
-                textAlign: "center",
-                fontSize: "35px",
-                border: "3px solid black",
-                lineHeight: "30vh",
-              }}
-            >
-              大波暁斗（〇〇薬局）
-            </Box>
-            <Box
-              sx={{
-                mt: 5,
-                mb: 7,
-                width: "80vw",
-                height: "30vh",
-                backgroundImage: "url()",
-                backgroundRepeat: "no-repeat",
-                backgroundColor: "white",
-                backgroundPosition: "bottom",
-                backgroundSize: "250px",
-                color: "green",
-                textAlign: "center",
-                fontSize: "35px",
-                border: "3px solid black",
-                lineHeight: "30vh",
-              }}
-            >
-              大波暁斗（〇〇医院）
-            </Box>
-          </Box>
-        </Box>
-        {/* </Grid> */}
-      </Grid>
-    </ThemeProvider>
+        グループ作成
+      </Box>
+      <Search data={lists} setData={setLists} />
+      <Box
+        sx={{
+          paddingTop: "20px",
+          overflowY: "scroll",
+          maxHeight: "calc(100% - 64px - 60px - 12px - 47px)",
+          msOverflowStyle: "none",
+          "::-webkit-scrollbar": { display: "none" },
+          px: "40px",
+        }}
+      >
+        {listsLength ? (
+          lists.map((list, index) => {
+            return (
+              list.isVisible && (
+                <Box
+                  sx={{
+                    height: height,
+                    backgroundColor: "white",
+                    color: "green",
+                    textAlign: "center",
+                    fontSize: "35px",
+                    border: "3px solid black",
+                    my: "5px",
+                    lineHeight: height,
+                    cursor: "pointer",
+                    borderRadius: "20px",
+                    backgroundColor: list.isSelected ? "#42B7DC" : "white",
+                  }}
+                  key={index}
+                  onClick={() => onClick(index)}
+                >
+                  {list.name}({list.belong})
+                </Box>
+              )
+            );
+          })
+        ) : (
+          <Typography sx={{ textAlign: "center", fontSize: "35px" }}>
+            該当担当者・施設は存在しません。
+            <br />
+            正しく入力してください
+          </Typography>
+        )}
+      </Box>
+      <Box sx={{ display: "flex", justifyContent: "center", mb: "5px" }}>
+        <Button variant="contained" sx={{ fontSize: "20px" }}>
+          作成
+        </Button>
+      </Box>
+    </Box>
   );
-}
+};
+
+export default Page;

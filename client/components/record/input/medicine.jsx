6d88ac281f30eae6842ed83349d7cd04773d70a6
Kotaro0722 <kotaro.tanaka@4nonome.com>
2023-12-17 11:59:46 +0900

    カルテ作成中

--- client/components/record/input/medicine.jsx
+++ client/components/record/input/medicine.jsx
@@ -1,32 +1,102 @@
 import {
+  Box,
   Button,
+  MenuItem,
   Paper,
+  Select,
   TextField,
+  InputLabel,
   ThemeProvider,
   Typography,
   createTheme,
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  Input,
 } from "@mui/material";
 
-const theme = createTheme({
-  components: {
-    MuiOutlinedInput: {
-      styleOverrides: {
-        root: {
-          height: "100%",
+const MedicineInput = () => {
+  const medicineAmount = [1, 2, 3, 4];
+
+  const theme = createTheme({
+    components: {
+      MuiOutlinedInput: {
+        styleOverrides: {
+          input: {
+            padding: "5px",
+          },
+        },
+      },
+      MuiFormControl: {
+        styleOverrides: {
+          root: {
+            display: "block",
+          },
         },
       },
     },
-  },
-});
-
-const MedicineInput = () => {};
+  });
+  return (
+    <Box sx={{ border: "1px solid black", p: "10px", borderRadius: "20px" }}>
+      <ThemeProvider theme={theme}>
+        <FormControl>
+          <InputLabel>処方薬名</InputLabel>
+          <Input />
+        </FormControl>
+        <FormControl>
+          <FormLabel sx={{ mr: "5px" }}>一日服用量</FormLabel>
+          <Select defaultValue={""}>
+            {medicineAmount.map((amount, index) => {
+              return (
+                <MenuItem value={amount} key={index}>
+                  {amount}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+        <FormControl>
+          <FormLabel>服用のタイミング</FormLabel>
+          <RadioGroup
+            row
+            aria-labelledby="demo-radio-buttons-group-label"
+            name="radio-buttons-group"
+          >
+            <FormControlLabel value="食前" control={<Radio />} label="食前" />
+            <FormControlLabel value="食間" control={<Radio />} label="食間" />
+            <FormControlLabel value="食後" control={<Radio />} label="食後" />
+            <FormControlLabel
+              value="就寝前"
+              control={<Radio />}
+              label="就寝前"
+            />
+          </RadioGroup>
+        </FormControl>
+      </ThemeProvider>
+    </Box>
+  );
+};
 
 const Medicine = () => {
+  const theme = createTheme({
+    components: {
+      MuiOutlinedInput: {
+        styleOverrides: {
+          root: {
+            height: "100%",
+          },
+        },
+      },
+    },
+  });
+
   return (
     <ThemeProvider theme={theme}>
       <Paper sx={{ px: "20px", height: "30%" }}>
         <Typography sx={{ fontSize: "25px" }}>処方薬</Typography>
-        {/* <Medicine /> */}
+        <MedicineInput />
         {/* <Button /> */}
       </Paper>
     </ThemeProvider>

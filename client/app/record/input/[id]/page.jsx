f8f18e29e2861c79d9cbbfcacdf08c9c7cfdb879
TanakaKotaro <kotaro.tanaka@4nonome.com>
2023-12-19 17:17:43 +0900

    データ修正

--- client/app/record/input/[id]/page.jsx
+++ client/app/record/input/[id]/page.jsx
@@ -152,7 +152,19 @@ const Page = ({ params }) => {
       date: {
         year: "2023",
         month: "12",
-        day: "17",
+        day: "07",
+      },
+      symptom: "関節が痛い",
+      medicine: [
+        { name: "ぺラックT錠", amount: 3, timing: "食後", note: "" },
+        { name: "トローチ", amount: 3, timing: "就寝前", note: "" },
+      ],
+    },
+    {
+      date: {
+        year: "2023",
+        month: "11",
+        day: "30",
       },
       symptom: "喉が痛い 発熱",
       medicine: [
@@ -163,8 +175,8 @@ const Page = ({ params }) => {
     {
       date: {
         year: "2023",
-        month: "12",
-        day: "17",
+        month: "11",
+        day: "23",
       },
       symptom: "喉が痛い 発熱",
       medicine: [
@@ -175,20 +187,8 @@ const Page = ({ params }) => {
     {
       date: {
         year: "2023",
-        month: "12",
-        day: "17",
-      },
-      symptom: "喉が痛い 発熱",
-      medicine: [
-        { name: "ぺラックT錠", amount: 3, timing: "食後", note: "" },
-        { name: "トローチ", amount: 3, timing: "就寝前", note: "" },
-      ],
-    },
-    {
-      date: {
-        year: "2023",
-        month: "12",
-        day: "17",
+        month: "11",
+        day: "16",
       },
       symptom: "喉が痛い 発熱",
       medicine: [

45be33f78db088a409a5e0c2396c358634c3e0b2
Kotaro0722 <kotaro.tanaka@4nonome.com>
2023-12-29 00:29:21 +0900

    テーブル修正

--- client/components/dashboard/question/questionTable.jsx
+++ client/components/dashboard/question/questionTable.jsx
@@ -1,156 +1,85 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import { useState } from "react";
 import {
-  Box,
-  Typography,
-  createTheme,
-  ThemeProvider,
-  IconButton,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Paper,
 } from "@mui/material";
 import exclamation from "@/public/img/exclamation.svg";
-import clock from "@/public/img/clock.svg";
-import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import { useRouter } from "next/navigation";
 import { paths } from "@/paths";
 
-const PageManager = ({ page, setPage, maxPage }) => {
-  const handleChangePage = (newPage) => {
+const QuestionTable = ({ data }) => {
+  const router = useRouter();
+  const [page, setPage] = useState(0);
+  const rowsPerPage = 10;
+  const now = new Date();
+
+  const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
-  return (
-    <Box sx={{ display: "flex", justifyContent: "space-around" }}>
-      <IconButton
-        disabled={page == 0}
-        onClick={() => handleChangePage(page - 1)}
-      >
-        <ArrowBackIosNewIcon />
-      </IconButton>
-      <Typography sx={{ lineHeight: "40px" }}>{page + 1}</Typography>
-      <IconButton
-        disabled={page == maxPage}
-        onClick={() => handleChangePage(page + 1)}
-      >
-        <ArrowForwardIosIcon />
-      </IconButton>
-    </Box>
-  );
-};
-
-const QuestionTable = ({ data, setData }) => {
-  const [page, setPage] = useState(0);
-  const [showPerson, setShowPerson] = useState(0);
-  const rowsPerPage = 5;
-  const router = useRouter();
-
-  const theme = createTheme({
-    components: {
-      MuiTableCell: {
-        styleOverrides: {
-          root: {
-            fontSize: "20px",
-            border: "none",
-          },
-        },
-      },
-      MuiTypography: {
-        styleOverrides: {
-          root: {
-            fontSize: "20px",
-          },
-        },
-      },
-    },
-  });
 
   return (
     <>
-      <Box
-        sx={{
-          width: "80%",
-          mx: "auto",
-          my: "30px",
-          maxWidth: "960px",
-          display: "flex",
-          flexDirection: "column",
-          gap: "20px",
-        }}
-      >
-        <ThemeProvider theme={theme}>
-          {data.map((datum, index) => {
-            const handleRowClick = () => {
-              const newData = data;
-              newData[index].isRead = true;
-              setData(newData);
-              setShowPerson(index);
-              router.push(paths.chat.patient(datum.id));
-            };
-            return (
-              rowsPerPage * page <= index &&
-              rowsPerPage * (page + 1) > index && (
-                <Box
-                  sx={{
-                    position: "relative",
-                    border: "5px solid black",
-                    borderRadius: "15px",
-                    display: "flex",
-                    justifyContent: "space-between",
-                    gap: "40px",
-                    py: "2px",
-                    cursor: "pointer",
-                  }}
+      <Paper sx={{ width: "600px", mx: "auto", mt: "30px" }}>
+        <Table aria-labelledby="tableTitle">
+          <TableBody>
+            {data.map((datum, index) => {
+              const handleRowClick = () => {
+                router.push(paths.chat.patient(datum.id));
+              };
+              return (
+                <TableRow
+                  hover
+                  onClick={() => handleRowClick(index)}
                   key={datum.id}
-                  onClick={handleRowClick}
+                  sx={{ cursor: "pointer" }}
                 >
-                  {!datum.isRead && (
+                  <TableCell
+                    sx={{
+                      width: "10px",
+                      p: "0px",
+                      border: "0px",
+                    }}
+                  ></TableCell>
+                  <TableCell>
                     <img
                       src={exclamation.src}
-                      alt="notRead"
-                      style={{
-                        width: "45px",
-                        position: "absolute",
-                        top: "13px",
-                        left: "20px",
-                      }}
-                    />
-                  )}
-                  <Typography
-                    sx={{
-                      ml: "85px",
-                      lineHeight: "70px",
-                      fontSize: "60px",
-                    }}
-                  >
-                    {datum.name}さんから
-                  </Typography>
-                  <Box
-                    sx={{
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      mr: "30px",
-                    }}
-                  >
-                    <img
-                      src={clock.src}
-                      alt="clock"
+                      alt="!"
                       style={{ width: "30px" }}
                     />
-                    <Typography sx={{ fontSize: "25px" }}>二日前</Typography>
-                  </Box>
-                </Box>
-              )
-            );
-          })}
-        </ThemeProvider>
-      </Box>
-      <PageManager
-        page={page}
-        setPage={setPage}
-        maxPage={Math.floor(data.length / rowsPerPage)}
-      />
+                  </TableCell>
+                  <TableCell component="td" scope="datum" padding="none">
+                    {datum.name}
+                  </TableCell>
+                  <TableCell align="center">{datum.content}</TableCell>
+                  <TableCell align="center">二日前</TableCell>
+                  <TableCell
+                    sx={{
+                      width: "10px",
+                      p: "0px",
+                      border: "0px",
+                    }}
+                  ></TableCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+        <TablePagination
+          rowsPerPageOptions={[]}
+          component="div"
+          count={data.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+        />
+      </Paper>
     </>
   );
 };
